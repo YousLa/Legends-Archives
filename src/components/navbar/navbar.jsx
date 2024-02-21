9585fa957b2c95d2f76a786c48dfd66e4658a3f6
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-21 11:32:37 +0100

    NavBar + fonts

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,76 @@
+import "./navbar.css"
+import Logo from "../../assets/Logo.png"
+import { useState } from "react";
+
+const Navbar = () => {
+
+    const [toggle, showMenu] = useState(false);
+
+    return (
+        <>
+            <aside className={toggle ? "aside show-menu" : "aside"}>
+                <a href="#home">
+                    <img src={Logo} alt="Legends Archives Logo" />
+                </a>
+
+                <nav className="nav">
+
+                    <div className="nav-menu">
+                        <ul className="nav-list">
+
+                            {/* Home */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Home
+                                </a>
+                            </li>
+
+                            {/* Champions */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Champions
+                                </a>
+                            </li>
+
+                            {/* Game Mode */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Game Mode
+                                </a>
+                            </li>
+
+                            {/* Ranked */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Ranked
+                                </a>
+                            </li>
+
+                            {/* Summoner */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Summoner
+                                </a>
+                            </li>
+
+                            {/* Player */}
+                            <li className="nav-item">
+                                <a href="#">
+                                    Player
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </nav>
+            </aside>
+
+            <div className={toggle ? "nav_toggle nav_toggle-open" : "nav_toggle"} onClick={() => showMenu(!toggle)}>
+                <i className="icon-menu"></i>
+            </div>
+
+        </>
+    )
+
+}
