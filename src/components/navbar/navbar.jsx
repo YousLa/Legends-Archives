d2a2122d59a3a6832f163b21ab54d87f3358db47
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-21 16:58:34 +0100

    Navbar debut style css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./navbar.css"
-import Logo from "../../assets/img/Logo.png"
+import Logo from "../../assets/img/Logo-Original.png"
 
 const Navbar = () => {
 
@@ -8,9 +8,9 @@ const Navbar = () => {
 
     return (
         <>
-            <div className={toggle ? "sticky show-menu" : "sticky"}>
-                <a href="/" >
-                    <img className="nav_logo" src={Logo} alt="Legends Archives Logo" />
+            <div className={toggle ? "fixed show-menu" : "fixed"}>
+                <a href="/" className="link-logo">
+                    <img className="nav-logo" src={Logo} alt="Legends Archives Logo" />
                 </a>
 
                 <nav className="nav">
@@ -19,42 +19,42 @@ const Navbar = () => {
 
                             {/* Home */}
                             <li className="nav-item">
-                                <a href="/">
+                                <a href="/" className="nav-link">
                                     Home
                                 </a>
                             </li>
 
                             {/* Champions */}
                             <li className="nav-item">
-                                <a href="/champions">
+                                <a href="/champions" className="nav-link">
                                     Champions
                                 </a>
                             </li>
 
                             {/* Game Mode */}
                             <li className="nav-item">
-                                <a href="/gamemode">
+                                <a href="/gamemode" className="nav-link">
                                     Game Mode
                                 </a>
                             </li>
 
                             {/* Ranked */}
                             <li className="nav-item">
-                                <a href="/ranked">
+                                <a href="/ranked" className="nav-link">
                                     Ranked
                                 </a>
                             </li>
 
                             {/* Summoner */}
                             <li className="nav-item">
-                                <a href="/summoner">
+                                <a href="/summoner" className="nav-link">
                                     Summoner
                                 </a>
                             </li>
 
                             {/* Player */}
                             <li className="nav-item">
-                                <a href="/player">
+                                <a href="/player" className="nav-link">
                                     Player
                                 </a>
                             </li>
